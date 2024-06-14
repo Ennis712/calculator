54964b190c6d9405132b5e36d4ae4ab332e45846
Ennis712 <165476222+Ennis712@users.noreply.github.com>
2024-06-14 10:43:52 +0800

    fist commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// 計算機運算
+
+System.Console.Write("請輸入第一個數字：");
+double num1 = System.Convert.ToDouble(System.Console.ReadLine());
+
+System.Console.Write("請輸入要做的運算符號：");
+string oper = System.Console.ReadLine();
+
+System.Console.Write("請輸入第二個數字：");
+double num2 = System.Convert.ToDouble(System.Console.ReadLine());
+
+if (oper == "+")
+{
+    System.Console.WriteLine(num1 + num2);
+}
+
+else if (oper == "-") 
+{
+    System.Console.WriteLine(num1 - num2);     
+}
+
+else if (oper == "*")
+{
+    System.Console.WriteLine(num1 * num2);
+}
+
+else if (oper == "/")
+{
+    System.Console.WriteLine(num1 / num2);
+}
+
+else
+{
+    System.Console.WriteLine("錯誤的運算符號");
+}
